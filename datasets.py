8a3f61b2e588df13acadfa45e17b90447c89ce60
CHRISTY JO MANTHARA <100943241+christymanthara@users.noreply.github.com>
2025-04-25 14:18:40 +0200

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,12 +1,27 @@
 datasets = {
     "pancreas": [
         "https://file.biolab.si/tsne-embedding/baron_2016h.h5ad",
-        "https://file.biolab.si/tsne-embedding/xin_2016.h5ad"
+        "https://file.biolab.si/tsne-embedding/xin_2016.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/GSM2230757_human1_umifm_counts_human.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/GSM2230758_human2_umifm_counts_human.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/GSM2230759_human3_umifm_counts_human.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/GSM2230760_human4_umifm_counts_human.h5ad"
     ],
     "brain": [
         "https://file.biolab.si/tsne-embedding/hrvatin_2018.h5ad",
         "https://file.biolab.si/tsne-embedding/chen_2017.h5ad",
+    ],
+    "retina":[
         "https://file.biolab.si/tsne-embedding/macosko_2015.h5ad",
         "https://file.biolab.si/tsne-embedding/shekhar_2016.h5ad"
-    ]
+    ],
+    "lung":[
+        "https://file.biolab.si/batch-effect-analysis/sample_proc_lung_test.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/sample_proc_lung_train.h5ad"
+    ],
+    "mouse-neuron":[
+        "https://file.biolab.si/batch-effect-analysis/GSM2230761_mouse1_umifm_counts_mouse.h5ad",
+        "https://file.biolab.si/batch-effect-analysis/GSM2230762_mouse2_umifm_counts_mouse.h5ad"
+    ],
+
 }
